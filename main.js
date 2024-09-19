cf4af8e82baac66b66e2936b313c577c711b26b7
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-09-19 18:45:36 +0700

    idcamp javascript 1

---
+++ main.js
@@ -0,0 +1,16 @@
+// TODO: buatlah variabel (konstan) bernama `currency` dan isi dengan nilai "IDR".
+const currency = "IDR"
+console.log(currency)
+// TODO: buatlah variabel bernama `value` dan isi dengan nilai 10000.
+let value = 10000
+console.log(value)
+// TODO: tambahkan nilai di dalam variabel `value` sebesar 5000.
+value += 5000
+console.log(value)
+
+/**
+ * TODO: buatlah variabel (konstan) bernama `money`,
+ * isi dengan penambahan string dari nilai `currency` + " " + `value`.
+ */
+const money = `${currency} ${value}`
+console.log(money)
